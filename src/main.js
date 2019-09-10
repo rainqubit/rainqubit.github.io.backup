1c47927c76a20447b0c5e2ff5def2d35103a3064
rainqubit <rainqubit@gmail.com>
2019-09-10 19:33:42 +0700

    added particle effect and fix positioning

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VueParticle from 'vue-particles'
 
 Vue.config.productionTip = false
-
+Vue.use(VueParticle)
 new Vue({
   store,
   render: h => h(App)
